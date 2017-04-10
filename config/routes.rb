ab8d7c67daedc44f76b70cf044bf21f22fab388c
Livia Puppa <liviapuppa@gmail.com>
2017-04-10 16:54:11 -0300

    game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/game', to: 'pages#game'
+
+  post '/score', to: 'pages#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
